5a25e0e50c8dc808e4e8084788c5225a408a5d19
Yanyu <jkf95338@gmail.com>
2024-01-12 19:02:46 +0800

    Progress 45:55

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
   const links = [
@@ -21,11 +22,9 @@ const Links = () => {
   ];
 
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
+        <NavLink item={link} key={link.title} />
       ))}
     </div>
   );
---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,15 @@
+import { usePathname } from "next/navigation";
+import styles from "./navLink.module.css";
+import Link from "next/link";
+
+const NavLink = ({ item }) => {
+    const pathName = usePathname();
+
+  return (
+    <Link href={item.path} key={item.title} className={`${styles.container}` `${pathName === item.title && styles.active}`}>
+      {item.title}
+    </Link>
+  );
+};
+
+export default NavLink;
